9e6cbee18c42e15dcfb1e709c5a45db328f08d42
Mahboob <mahboob@primetrader.com>
2026-05-01 18:16:39 +0500

    footer styling

--- docs/.vitepress/theme/Footer.vue
+++ docs/.vitepress/theme/Footer.vue
@@ -1,98 +1,169 @@
 <script setup>
-import { withBase } from 'vitepress'
+import { withBase } from "vitepress";
 </script>
 
 <template>
   <footer class="bitxpay-footer">
     <div class="footer-container">
       <!-- Company Section -->
-      <div class="footer-section">
+      <div class="footer-left">
+      <div class="footer-logo-container">
+        <div class="footer-logo">
+          <img
+            :src="withBase('/icons/bitxpay.svg')"
+            alt="BITXpay"
+            class="footer-logo-img"
+          />
+        </div>
         <h3 class="footer-title">BITXpay</h3>
+      </div>
+
         <p class="footer-description">
-          Enterprise-grade cryptocurrency payment gateway. Move value globally with speed, security, and full compliance.
+          Enterprise-grade cryptocurrency payment gateway. Move value globally
+          with speed, security, and full compliance.
         </p>
         <div class="footer-social">
-          <a href="https://github.com/bitxpay" target="_blank" rel="noopener" aria-label="GitHub">
+          <a
+            href="https://github.com/bitxpay"
+            target="_blank"
+            rel="noopener"
+            aria-label="GitHub"
+          >
             <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+              <path
+                d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+              />
             </svg>
           </a>
-          <a href="https://discord.gg/bitxpay" target="_blank" rel="noopener" aria-label="Discord">
+          <a
+            href="https://discord.gg/bitxpay"
+            target="_blank"
+            rel="noopener"
+            aria-label="Discord"
+          >
             <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515a.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0a12.64 12.64 0 0 0-.617-1.25a.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057a19.9 19.9 0 0 0 5.993 3.03a.078.078 0 0 0 .084-.028a14.09 14.09 0 0 0 1.226-1.994a.076.076 0 0 0-.041-.106a13.107 13.107 0 0 1-1.872-.892a.077.077 0 0 1-.008-.128a10.2 10.2 0 0 0 .372-.292a.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127a12.299 12.299 0 0 1-1.873.892a.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028a19.839 19.839 0 0 0 6.002-3.03a.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.956-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.42c0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.955-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.42c0 1.333-.946 2.418-2.157 2.418z"/>
+              <path
+                d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515a.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0a12.64 12.64 0 0 0-.617-1.25a.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057a19.9 19.9 0 0 0 5.993 3.03a.078.078 0 0 0 .084-.028a14.09 14.09 0 0 0 1.226-1.994a.076.076 0 0 0-.041-.106a13.107 13.107 0 0 1-1.872-.892a.077.077 0 0 1-.008-.128a10.2 10.2 0 0 0 .372-.292a.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127a12.299 12.299 0 0 1-1.873.892a.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028a19.839 19.839 0 0 0 6.002-3.03a.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.956-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.42c0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.955-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.42c0 1.333-.946 2.418-2.157 2.418z"
+              />
             </svg>
           </a>
-          <a href="https://twitter.com/bitxpay" target="_blank" rel="noopener" aria-label="Twitter">
+          <a
+            href="https://twitter.com/bitxpay"
+            target="_blank"
+            rel="noopener"
+            aria-label="Twitter"
+          >
             <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/>
+              <path
+                d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"
+              />
             </svg>
           </a>
         </div>
       </div>
 
-      <!-- Documentation Section -->
-      <div class="footer-section">
-        <h4 class="footer-heading">Documentation</h4>
-        <ul class="footer-links">
-          <li><a :href="withBase('/get-started/')">Get Started</a></li>
-          <li><a :href="withBase('/get-started/quick-start')">Quick Start</a></li>
-          <li><a :href="withBase('/get-started/authentication')">Authentication</a></li>
-          <li><a :href="withBase('/get-started/supported-networks')">Supported Networks</a></li>
-          <li><a :href="withBase('/api-reference/')">API Reference</a></li>
-        </ul>
-      </div>
+      <div class="footer-right">
+        <!-- Documentation Section -->
+        <div class="footer-section">
+          <h4 class="footer-heading">Documentation</h4>
+          <ul class="footer-links">
+            <li><a :href="withBase('/get-started/')">Get Started</a></li>
+            <li>
+              <a :href="withBase('/get-started/quick-start')">Quick Start</a>
+            </li>
+            <li>
+              <a :href="withBase('/get-started/authentication')"
+                >Authentication</a
+              >
+            </li>
+            <li>
+              <a :href="withBase('/get-started/supported-networks')"
+                >Supported Networks</a
+              >
+            </li>
+            <li><a :href="withBase('/api-reference/')">API Reference</a></li>
+          </ul>
+        </div>
 
-      <!-- Resources Section -->
-      <div class="footer-section">
-        <h4 class="footer-heading">Resources</h4>
-        <ul class="footer-links">
-          <li><a :href="withBase('/get-started/sdks-libraries')">SDKs & Libraries</a></li>
-          <li><a :href="withBase('/get-started/webhooks')">Webhooks</a></li>
-          <li><a :href="withBase('/testing/')">Testing Tools</a></li>
-          <li><a :href="withBase('/integration/environments')">Environments</a></li>
-          <li><a :href="withBase('/security/best-practices')">Security</a></li>
-        </ul>
-      </div>
+        <!-- Resources Section -->
+        <div class="footer-section">
+          <h4 class="footer-heading">Resources</h4>
+          <ul class="footer-links">
+            <li>
+              <a :href="withBase('/get-started/sdks-libraries')"
+                >SDKs & Libraries</a
+              >
+            </li>
+            <li><a :href="withBase('/get-started/webhooks')">Webhooks</a></li>
+            <li><a :href="withBase('/testing/')">Testing Tools</a></li>
+            <li>
+              <a :href="withBase('/integration/environments')">Environments</a>
+            </li>
+            <li>
+              <a :href="withBase('/security/best-practices')">Security</a>
+            </li>
+          </ul>
+        </div>
 
-      <!-- Support Section -->
-      <div class="footer-section">
-        <h4 class="footer-heading">Support</h4>
-        <ul class="footer-links">
-          <li><a :href="withBase('/testing/troubleshooting')">Troubleshooting</a></li>
-          <li><a href="https://github.com/bitxpay/issues" target="_blank">Report Issues</a></li>
-          <li><a href="https://discord.gg/bitxpay" target="_blank">Discord Community</a></li>
-          <li><a href="mailto:support@bitxpay.com">Email Support</a></li>
-          <li><a :href="withBase('/contribution/')">Contribute</a></li>
-        </ul>
-      </div>
+        <!-- Support Section -->
+        <div class="footer-section">
+          <h4 class="footer-heading">Support</h4>
+          <ul class="footer-links">
+            <li>
+              <a :href="withBase('/testing/troubleshooting')"
+                >Troubleshooting</a
+              >
+            </li>
+            <li>
+              <a href="https://github.com/bitxpay/issues" target="_blank"
+                >Report Issues</a
+              >
+            </li>
+            <li>
+              <a href="https://discord.gg/bitxpay" target="_blank"
+                >Discord Community</a
+              >
+            </li>
+            <li><a href="mailto:support@bitxpay.com">Email Support</a></li>
+            <li><a :href="withBase('/contribution/')">Contribute</a></li>
+          </ul>
+        </div>
 
-      <!-- Company Section -->
-      <div class="footer-section">
-        <h4 class="footer-heading">Company</h4>
-        <ul class="footer-links">
-          <li><a href="https://bitx.es/" target="_blank">About BITXpay</a></li>
-          <li><a href="https://bitx.es/contact-us" target="_blank">Contact Us</a></li>
-          <li><a :href="withBase('/security/')">Security</a></li>
-          <li><a href="https://bitx.es/privacy-policy" target="_blank">Privacy Policy</a></li>
-          <li><a href="https://bitx.es/terms-and-conditions" target="_blank">Terms of Service</a></li>
-        </ul>
+        <!-- Company Section -->
+        <div class="footer-section">
+          <h4 class="footer-heading">Company</h4>
+          <ul class="footer-links">
+            <li>
+              <a href="https://bitx.es/" target="_blank">About BITXpay</a>
+            </li>
+            <li>
+              <a href="https://bitx.es/contact-us" target="_blank"
+                >Contact Us</a
+              >
+            </li>
+            <li><a :href="withBase('/security/')">Security</a></li>
+            <li>
+              <a href="https://bitx.es/privacy-policy" target="_blank"
+                >Privacy Policy</a
+              >
+            </li>
+            <li>
+              <a href="https://bitx.es/terms-and-conditions" target="_blank"
+                >Terms of Service</a
+              >
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
 
     <!-- Bottom Bar -->
     <div class="footer-bottom">
-      <div class="footer-container">
-        <div class="footer-bottom-content">
-          <p class="footer-copyright">
-            © {{ new Date().getFullYear() }} BITXpay. All rights reserved.
-          </p>
-          <div class="footer-bottom-links">
-            <span class="footer-status">
-              <span class="status-dot"></span>
-              All Systems Operational
-            </span>
-          </div>
-        </div>
+      <div class="footer-bottom-inner">
+        <p class="footer-copyright">
+          © {{ new Date().getFullYear() }} BITXpay. All rights reserved.
+        </p>
+        <span class="footer-status"> All Systems Operational </span>
       </div>
     </div>
   </footer>
@@ -100,64 +171,41 @@ import { withBase } from 'vitepress'
 
 <style>
 .bitxpay-footer {
-  background: var(--bitx-primary);
+  background: #040c17;
   color: var(--bitx-gray-200);
-  border-top: 1px solid var(--bitx-primary-light);
-  margin-top: 4rem;
 }
 
 .footer-container {
-  max-width: 1280px;
+  width: 100%;
   margin: 0 auto;
   padding: 3rem 2rem;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  gap: 4rem;
+}
+
+/* Left: brand block */
+.footer-left {
+  flex: 0 0 280px;
+  max-width: 280px;
+}
+
+/* Right: four link columns */
+.footer-right {
   display: grid;
-  grid-template-columns: 1.5fr repeat(4, 1fr);
-  gap: 3rem;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 2rem;
+  justify-items: start;
 }
 
 .footer-brand {
   grid-column: span 1;
 }
 
-.footer-title {
-  font-size: 1.5rem;
-  font-weight: 800;
-  color: var(--bitx-gray-50);
-  margin: 0 0 1rem 0;
-  background: linear-gradient(120deg, var(--bitx-accent-light), var(--bitx-gold-light));
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  background-clip: text;
-}
-
-.footer-description {
-  font-size: 0.9rem;
-  line-height: 1.6;
-  color: var(--bitx-gray-300);
-  margin: 0 0 1.5rem 0;
-}
-
 .footer-social {
   display: flex;
   gap: 1rem;
-}
-
-.footer-social a {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 36px;
-  height: 36px;
-  border-radius: 8px;
-  background: var(--bitx-primary-light);
-  color: var(--bitx-gray-300);
-  transition: all 0.3s ease;
-}
-
-.footer-social a:hover {
-  background: var(--bitx-accent);
-  color: white;
-  transform: translateY(-2px);
 }
 
 .footer-heading {
@@ -194,7 +242,25 @@ import { withBase } from 'vitepress'
 
 .footer-bottom {
   border-top: 1px solid var(--bitx-primary-light);
-  padding: 1.5rem 0;
+  align-items: center;
+  background-color: #000000;
+}
+
+.footer-bottom-inner {
+  margin: 0 auto;
+  padding: 1.5rem 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+}
+
+.footer-status {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 0.875rem;
+  color: var(--bitx-gray-400);
 }
 
 .footer-bottom-content {
@@ -235,11 +301,11 @@ import { withBase } from 'vitepress'
   .footer-container {
     grid-template-columns: repeat(3, 1fr);
   }
-  
+
   .footer-brand {
     grid-column: span 3;
   }
-  
+
   .footer-cta {
     flex-direction: row;
   }
@@ -250,21 +316,21 @@ import { withBase } from 'vitepress'
     grid-template-columns: repeat(2, 1fr);
     gap: 2rem;
   }
-  
+
   .footer-brand {
     grid-column: span 2;
   }
-  
+
   .footer-cta {
     flex-direction: column;
   }
-  
+
   .footer-bottom-content {
     flex-direction: column;
     gap: 1rem;
     text-align: center;
   }
-  
+
   .footer-compliance {
     justify-content: center;
   }
@@ -274,7 +340,7 @@ import { withBase } from 'vitepress'
   .footer-container {
     grid-template-columns: 1fr;
   }
-  
+
   .footer-brand {
     grid-column: span 1;
   }
