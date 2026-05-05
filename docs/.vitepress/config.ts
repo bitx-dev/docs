d56bdf4d69ebd6709c789fd733ca203d3a39a71f
Mahboob <mahboob@primetrader.com>
2026-05-05 17:20:02 +0500

    added favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -19,8 +19,8 @@ export default defineConfig({
   // Head tags for SEO
   head: [
     ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
-    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
-    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/logo.svg' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/logo.svg' }],
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
     ['meta', { name: 'theme-color', content: '#3b82f6' }],
     ['meta', { property: 'og:type', content: 'website' }],
